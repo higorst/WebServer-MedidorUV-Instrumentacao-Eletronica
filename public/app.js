d352f8a470fef66a9cf0fece4630ed4be86a5475
Higor <higor-sj@hotmail.com>
2019-12-01 20:19:04 -0300

    finish

---
+++ public/app.js
@@ -0,0 +1,9 @@
+// src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"
+// axios.get('http://localhost:3000/')
+//     .then(response => sendValue(response.data))
+//     .catch(error => console.log(error))
+   
+// const sendValue = ( res ) => {
+//    const value = document.getElementById('value')
+//    value.value = res
+// }
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+let request = require('request');
+var path = require('path');
+
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/getUV', function(req, res) {
+   request('http://192.168.0.110/', function (error, response, body) {
+      res.send(body)
+   });
+});
+
+app.listen(3000, function() {
+   console.log('Rodando porta 3000');
+});
